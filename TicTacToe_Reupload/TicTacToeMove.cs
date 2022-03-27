cc473a8c7d80b93167beabbadf8b4a9fd81df443
T-Dudnikov <tikhomir.dudnikov@gmail.com>
2022-03-27 12:57:24 +0300

    Changed migration InitialCreate and applied it to a db.

--- TicTacToe_Reupload/TicTacToeMove.cs
+++ TicTacToe_Reupload/TicTacToeMove.cs
@@ -5,17 +5,17 @@ namespace TicTacToe_Reupload
     [Keyless]
     public class TicTacToeMove
     {
-        public int Game_id { get; set; }
-        public int Box1 { get; set; }
-        public int Box2 { get; set; }
-        public int Box3 { get; set; }
-        public int Box4 { get; set; }
-        public int Box5 { get; set; }
-        public int Box6 { get; set; }
-        public int Box7 { get; set; }
-        public int Box8 { get; set; }
-        public int Box9 { get; set; }
-        public int win { get; set; }
+        public int? Game_id { get; set; }
+        public int? Box1 { get; set; }
+        public int? Box2 { get; set; }
+        public int? Box3 { get; set; }
+        public int? Box4 { get; set; }
+        public int? Box5 { get; set; }
+        public int? Box6 { get; set; }
+        public int? Box7 { get; set; }
+        public int? Box8 { get; set; }
+        public int? Box9 { get; set; }
+        public int? win { get; set; }
 
     }
 }
